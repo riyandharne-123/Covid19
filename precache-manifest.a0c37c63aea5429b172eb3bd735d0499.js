fbe9802afe71dca5543502915ce3e08d894f0070
riyandharne-123 <rockydods123@gmail.com>
2020-05-06 16:59:23 +0530

    Updates

---
+++ precache-manifest.a0c37c63aea5429b172eb3bd735d0499.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b6224c1f941f0a5f76173905e6ad39e",
+    "url": "/riyandharne-123.gtihub.io/Covid19/index.html"
+  },
+  {
+    "revision": "4a96ca6d6a137d2f49ee",
+    "url": "/riyandharne-123.gtihub.io/Covid19/static/css/main.b67c2b12.chunk.css"
+  },
+  {
+    "revision": "5c9d21db24045f0ad253",
+    "url": "/riyandharne-123.gtihub.io/Covid19/static/js/2.1763b1d3.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/riyandharne-123.gtihub.io/Covid19/static/js/2.1763b1d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a96ca6d6a137d2f49ee",
+    "url": "/riyandharne-123.gtihub.io/Covid19/static/js/main.62d07e6e.chunk.js"
+  },
+  {
+    "revision": "d04fa4c2728be08a77f7",
+    "url": "/riyandharne-123.gtihub.io/Covid19/static/js/runtime-main.ef801d9a.js"
+  }
+]);
